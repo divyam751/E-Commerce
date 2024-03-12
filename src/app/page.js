3a56c194bb40a13c02f8cc2e27c69c510d1dc1fe
divyam751 <mi.divyam@gmail.com>
2024-03-12 11:26:46 +0530

    redirecting to home

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,12 @@
+"use client";
+
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    redirect("/home");
+  }, []);
   return (
     <main className="app">
       <h1>Home Route</h1>
